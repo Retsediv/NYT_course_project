4d55c6dd01455dfd5857523b26793878c4fd7418
retsediv <retsediv1@gmail.com>
2017-04-28 09:31:45 +0300

    Expand example of NYT API usage with data processing and writing into file

--- examples/nyt_archive_api.py
+++ examples/nyt_archive_api.py
@@ -1,5 +1,34 @@
 from nytimesarchive import ArchiveAPI
 
-api = ArchiveAPI('edfee37ae9e24f848d39bd3976afa7bd')
 
-print(api.query(2016, 11))
+def filter_with_geo(article):
+    for key in article['keywords']:
+        if key['name'] == 'glocations':
+            return True
+
+    return False
+
+
+def clear_articles(articles):
+    for article in articles:
+        keywords = article["keywords"]
+        pub_date = article["pub_date"]
+        headline = article["headline"]
+
+        article.clear()
+
+        article["keywords"] = keywords
+        article["pub_date"] = pub_date
+        article["headline"] = headline
+
+    return articles
+
+
+with open("data_samples.txt", "w") as f:
+    api = ArchiveAPI('edfee37ae9e24f848d39bd3976afa7bd')
+    articles = api.query(2016, 11)['response']['docs']
+    geo_articles = clear_articles(list(filter(filter_with_geo, articles))[0:2])
+
+    f.write(str(geo_articles))
+
+    print(geo_articles)
