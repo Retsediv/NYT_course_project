7b6c0bc820428b9b3be8acfaac3f8bc590cdb5fb
retsediv <retsediv1@gmail.com>
2017-05-25 13:57:42 +0300

    Make setup.py

---
+++ setup.py
@@ -0,0 +1,9 @@
+from setuptools import setup, find_packages
+
+setup(name="NYT_Reserach",
+      version="0.1.0",
+      packages=find_packages(),
+      scripts=["run.py"],
+      url="127.0.0.1:5000",
+      author="Andrew Zhuravchak",
+      author_email="zhuravchak@ucu.edu.ua")
